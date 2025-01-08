82ca63e4c8d073227d10e654a2978daef53e690b
juanmata06 <mataguzzojuan@gmail.com>
2025-01-08 16:06:54 +0100

    use of Output signals for getting values

--- src/app/calculator/components/button/button.component.ts
+++ src/app/calculator/components/button/button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, HostBinding, OnInit, ViewEncapsulation, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, OnInit, ViewEncapsulation, input, output, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -15,6 +15,12 @@ import { ChangeDetectionStrategy, Component, HostBinding, OnInit, ViewEncapsulat
   }
 })
 export class ButtonComponent {
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * General vars for component
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+  buttonValue = viewChild<ElementRef<HTMLButtonElement>>('button');
 
   //* Input signals:
   public isCommandButton = input(false, {
@@ -25,6 +31,9 @@ export class ButtonComponent {
     transform: (value: boolean | string) => typeof value == 'string' ? value == '' : value
   });
 
+  //* Output signals:
+  public onClickButton = output<string>();
+
   //* Host bindings:
   // @HostBinding('class.is-command-button') get CommandStyle() {
   //   return this.isCommandButton();
@@ -33,4 +42,40 @@ export class ButtonComponent {
   @HostBinding('class.w-2/4') get CommandStyle() {
     return this.isDoubleSize();
   }
+
+  /**
+   * -----------------------------------------------------------------------------------------------------------------------------
+   * LYFECYCLE METHODS
+   * -----------------------------------------------------------------------------------------------------------------------------
+   */
+
+  constructor() { }
+
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * PRIVATE METHODS
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * PRIVATE VALIDATION AND INTERNAL PROCESS METHODS
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * PUBLIC METHODS
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+  public emitButtonValue(): void {
+    if (!this.buttonValue()?.nativeElement) { return; }
+    this.onClickButton.emit(this.buttonValue()!.nativeElement.innerText.trim());
+  }
+
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * PUBLIC VALIDATION AND INTERNAL PROCESS METHODS
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
 }
