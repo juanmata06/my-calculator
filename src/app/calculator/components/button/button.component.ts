8c7d53a226208de0c19177dd8d60e18f9b3d1c2e
juanmata06 <mataguzzojuan@gmail.com>
2025-01-08 15:29:39 +0100

    buttons styles using InputSignal & HostBindings

--- src/app/calculator/components/button/button.component.ts
+++ src/app/calculator/components/button/button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, HostBinding, OnInit, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, OnInit, ViewEncapsulation, input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,18 +8,29 @@ import { ChangeDetectionStrategy, Component, HostBinding, OnInit, input } from '
     CommonModule,
   ],
   templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400'
   }
 })
-export class ButtonComponent{
+export class ButtonComponent {
 
+  //* Input signals:
   public isCommandButton = input(false, {
     transform: (value: boolean | string) => typeof value == 'string' ? value == '' : value
   });
 
-  @HostBinding('class.bg-indigo-700') get CommandStyle() {
-    return this.isCommandButton();
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) => typeof value == 'string' ? value == '' : value
+  });
+
+  //* Host bindings:
+  // @HostBinding('class.is-command-button') get CommandStyle() {
+  //   return this.isCommandButton();
+  // }
+
+  @HostBinding('class.w-2/4') get CommandStyle() {
+    return this.isDoubleSize();
   }
 }
